40bf876fdca4f47426d0992e57c875c38fa93f0e
dongwook98 <ehddnr8563@gmail.com>
2023-04-22 01:03:46 +0900

    이벤트 처리

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -3,8 +3,16 @@ import Profile from './components/Profile';
 import Avatar from './components/Avatar';
 
 export default function AppProfile() {
+  // 고차함수(함수 리턴)
+  const handleClick = (name) => (event) => {
+    console.log(name); // '강동욱'
+    console.log(event); // SyntheticBaseEvent { ... }
+    alert('클릭되었습니다!');
+  };
   return (
     <Fragment>
+      {/* 이벤트핸들러 자리에는 함수 참조값이 와야한다. 어떤 값을 전달하고 싶다면 함수를 리턴 */}
+      <button onClick={handleClick('강동욱')}>클릭</button>
       <Profile
         url='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
         name='강동욱'
