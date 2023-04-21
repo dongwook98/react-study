87cff9c8ab5ce9ac79132b8452ff1433ffac7217
dongwook98 <ehddnr8563@gmail.com>
2023-04-22 00:27:47 +0900

    jsx

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '동욱';
+  const list = ['우유', '딸기', '바나나', '초코'];
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h2>hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((el) => {
+          return <li>{el}</li>;
+        })}
+      </ul>
+      <img
+        style={{ width: '200px', height: 100 + 100 + 'px' }}
+        src='https://plus.unsplash.com/premium_photo-1670270204757-08153c6fd333?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        alt=''
+      />
+    </>
   );
 }
 
