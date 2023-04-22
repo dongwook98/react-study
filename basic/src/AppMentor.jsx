013c38985c4ad0b65e39bbaeaa9bdfff70b7c507
dongwook98 <ehddnr8563@gmail.com>
2023-04-22 23:05:31 +0900

    중첩 객체 상태 관리

---
+++ basic/src/AppMentor.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function AppMentor() {
+  const [person, setPerson] = useState({
+    name: '동욱',
+    title: '개발자',
+    mentor: {
+      name: '제로초',
+      title: '시니어개발자',
+    },
+  });
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p>
+        {person.name}멘토는 {person.mentor.name} ({person.mentor.title})
+      </p>
+      <button
+        onClick={() => {
+          const name = prompt(`what's your mentor's name?`);
+          setPerson((prev) => ({
+            ...prev,
+            mentor: { ...prev.mentor, name: name }, // name: name -> name으로 축약가능
+          }));
+        }}
+      >
+        멘토 이름 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          const title = prompt(`what's your mentor's name?`);
+          setPerson((prev) => ({
+            ...prev,
+            mentor: { ...prev.mentor, title: title },
+          }));
+        }}
+      >
+        멘토 타이틀 바꾸기
+      </button>
+    </div>
+  );
+}
