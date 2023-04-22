d5f49c6b4810377064f7e4c5c098fd4e2c3c6d11
dongwook98 <ehddnr8563@gmail.com>
2023-04-22 22:37:18 +0900

    연관된 데이터는 객체로 묶어 state에 보관

---
+++ basic/src/AppXY.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState } from 'react';
+import './AppXY.css';
+
+export default function AppXY() {
+  // const [x, setX] = useState(0);
+  // const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
+
+  const handleonPointerMove = (e) => {
+    console.log(e.clientX, e.clientY);
+    // setPosition({ x: e.clientX, y: e.clientY });
+    // 만약 수평으로만 이동이 가능하다면?
+    setPosition((prev) => ({ ...prev, x: e.clientX }));
+  };
+
+  return (
+    <div className='container' onPointerMove={handleonPointerMove}>
+      <div
+        className='pointer'
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+      ></div>
+    </div>
+  );
+}
--- basic/src/index.js
+++ basic/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppJSX from './AppJSX';
-import AppProfile from './AppProfile';
-import AppCounter from './AppCounter';
 import reportWebVitals from './reportWebVitals';
-import AppProducts from './AppProducts';
+import AppXY from './AppXY';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <AppProducts />
+  <AppXY />
   // </React.StrictMode>
 );
 
