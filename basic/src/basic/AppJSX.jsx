68f179d4d77824622e523acf93a056fa7aafef21
강동욱 <ehddnr8563@gmail.com>
2023-12-15 02:37:45 +0900

    add: JSX

--- basic/src/basic/AppJSX.jsx
+++ basic/src/basic/AppJSX.jsx
@@ -2,15 +2,31 @@ import './App.css';
 
 function AppJSX() {
   const name = '동욱';
-  const list = ['우유', '딸기', '바나나', '초코'];
+  const list = [
+    '우유',
+    '딸기',
+    '바나나',
+    '초코',
+    '키위',
+    '당근',
+    '수박',
+    '포도',
+  ];
+  /**
+   * JSX 문법 정리
+   * 1. 컴포넌트는 꼭 하나의 태그만 반환해야한다. 다수의 태그를 반환하고 싶다면 부모 태그를 감싸야한다.
+   * 2. class대신에 className 사용
+   * 3. JSX는 자바스크립트에서 작성하는 문법이기 때문에 자바스크립트 코드를 중간중간에 작성할 수 있다.
+   * 다만, 자바스크립트 코드를 작성할때는 중괄호{}로 묶어줘야한다.
+   */
   return (
     <>
       <h1 className='orange'>{`Hello! ${name}`}</h1>
       <h2>hello!</h2>
       <p>{name}</p>
       <ul>
-        {list.map((el, i) => {
-          return <li key={i}>{el}</li>;
+        {list.map((item, i) => {
+          return <li key={i}>{item}</li>;
         })}
       </ul>
       <img
