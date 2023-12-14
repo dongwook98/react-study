ea320f5cde6bbd98a8ffb1dcc6fb4c37931618a0
강동욱 <ehddnr8563@gmail.com>
2023-12-15 02:39:10 +0900

    add: useState

--- basic/src/basic/AppCounter.jsx
+++ basic/src/basic/AppCounter.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
 import Counter from './components/Counter';
+import './App.css';
 
 export default function AppCounter() {
-  const [count, setCount] = useState(0);
+  // 컴포넌트들끼리 공통적으로 필요한 데이터는 제일 근접한 부모에 State를 둔다.
+  const [totalCount, setTotalCount] = useState(0);
 
   const handleClick = () => {
-    setCount((prevCount) => prevCount + 1);
+    setTotalCount((prev) => prev + 1);
   };
   return (
     <div className='container'>
       <div className='banner'>
-        Total Count: {count} {count > 10 ? '🔥' : '❄️'}
+        Total Count: {totalCount} {totalCount > 10 ? '🔥' : '❄️'}
       </div>
       <div className='counters'>
-        <Counter total={count} onClick={handleClick} />
-        <Counter total={count} onClick={handleClick} />
+        {/* 외부에 전달하는 props 콜백함수 및 외부로 노출하는 API는 우리가 노출하고 싶은 범위 내에서
+          최대한 좁혀서 제공해 주는게 좋다. setTotalCount를 전달하지말고 handleClick으로 감싸서 전달
+        */}
+        <Counter total={totalCount} onClick={handleClick} />
+        <Counter total={totalCount} onClick={handleClick} />
       </div>
     </div>
   );
--- basic/src/basic/components/Counter.jsx
+++ basic/src/basic/components/Counter.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function Counter({ total, onClick }) {
-  const [number, setNumber] = useState(0);
-  // Counter 함수 다시 실행되어도 number는 0으로 초기화 x (useState는 다시 호출되어도 값을 계속 기억하고 있음)
+  // 리액트에서는 UI와 밀접하게 관련된 데이터는 State에 보관
+  const [count, setCount] = useState(0);
+  // Counter 함수 다시 실행되어도 count는 0으로 초기화X (useState는 다시 호출되어도 값을 계속 기억하고 있음)
+  /**
+   * 리액트에서 제공하는 setCount를 이용했기 때문에 count값이 변경이 될때마다 리액트가 자동적으로 Counter라는 함수 컴포넌트를 다시 호출한다.
+   * Counter에 props가 있으면 그 props가 변경되거나 내부에서 가지고 있는 useState의 State가 변경되면
+   * 리액트는 State가 변경될때 마다 변경된 해당 컴포넌트 함수 전체를 다시 실행한다.
+   * 그래서 처음부터 리턴되는 값 이런 DOM요소들이 생기는데
+   * 다만, 리액트에서는 Virtual DOM을 사용하기 때문에 이전의 DOM요소와 지금의 DOM요소에서 변경되는 부분,
+   * 즉 <p></p> 만 변경이 되서 <p></p>만 업데이트해준다.
+   */
 
   const HandleClick = () => {
-    // setNumber() 실행 -> Counter함수 실행
-    // 여러번 업데이트 하거나 이전 상태값을 토대로 업데이트하는 경우에는 기존에 스냅샷된 외부값에 의존하기 보다는 콜백함수형태로 setState하는것이 더 안전하다.
-    setNumber((prevNumber) => prevNumber + 1);
-    onClick(); // 외부 total값도 변경해줘야한다.
+    /**
+     * 여러번 업데이트 하거나 이전 상태값을 토대로 업데이트하는 경우에는
+     * 기존에 스냅샷된 외부값에 의존하기 보다는 콜백함수형태로 setState하는것이 더 안전하다.
+     */
+    setCount((prevCount) => prevCount + 1);
+    onClick(); // 외부 totalCount값도 변경
   };
   return (
     <div className='counter'>
       <p className='number'>
-        {number}
+        {count}
         <span className='total'> /{total} </span>
       </p>
       <button className='button' onClick={HandleClick}>
