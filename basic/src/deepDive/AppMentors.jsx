fd9f33b5058f94135217982a024b75bb771c830d
강동욱 <ehddnr8563@gmail.com>
2023-12-16 06:16:54 +0900

    add: 리액트 불변성, useReducer

--- basic/src/deepDive/AppMentors.jsx
+++ basic/src/deepDive/AppMentors.jsx
@@ -1,30 +1,61 @@
 import React from 'react';
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
 import { useState } from 'react';
-import personReducer from './deepDive/reducer/person-reducer';
+// import personReducer from './reducer/person-reducer';
 
 export default function AppMentors() {
-  // const [person, setPerson] = useState(initialPerson);
-  const [person, dispatch] = useReducer(personReducer, initialPerson);
+  /**
+   * 리액트 불변성
+   *
+   * 리액트에서 사용하는 모든 State는 불변성을 유지해야한다.
+   * 리액트에서 사용하는 상태에 객체나 배열의 내부 내용을 직접적으로 변경하면 안되고,
+   * 변경이 발생한다면 배열과 객체의 전체적인 껍데기를 새로 만들어서 업데이트 해줘야 한다.
+   */
+  const [person, setPerson] = useState(initialPerson);
+  /**
+   * useReducer
+   *
+   * useReducer를 사용하면 컴포넌트 내부에 필요한 로직들만 간직할 수 있고,
+   * 다른 컴포넌트에서도 동일한 로직이 필요하다면 그 로직을 재사용도 할 수 있다.
+   */
+  // const [person, dispatch] = useReducer(personReducer, initialPerson);
 
-  // 멘토 수정
+  // 멘토 이름 수정
   const handleUpdate = () => {
     const prev = prompt('누구의 이름을 바꾸고 싶은가요?');
     const current = prompt('이름을 무엇으로 바꾸고 싶은가요?');
-    dispatch({ type: 'updated', prev, current });
+    setPerson((person) => ({
+      ...person,
+      // map을 사용해서 새로운 mentors를 반환
+      mentors: person.mentors.map((mentor) => {
+        if (mentor.name === prev) {
+          return { ...mentor, name: current };
+        }
+        return mentor;
+      }),
+    }));
+    // dispatch({ type: 'updated', prev, current });
   };
 
   // 멘토 추가
   const handleAdd = () => {
     const name = prompt('추가할 멘토의 이름을 적어주세요.');
     const title = prompt('추가할 멘토의 직무를 적어주세요.');
-    dispatch({ type: 'added', name, title });
+    // dispatch({ type: 'added', name, title });
+    setPerson((person) => ({
+      ...person,
+      mentors: [{ name, title }, ...person.mentors],
+    }));
   };
 
   // 멘토 삭제
   const handleDelete = () => {
     const name = prompt('삭제할 멘토의 이름을 적어주세요.');
-    dispatch({ type: 'deleted', name });
+    // dispatch({ type: 'deleted', name });
+    setPerson((person) => ({
+      ...person,
+      mentors: person.mentors.filter((mentor) => mentor.name !== name),
+    }));
   };
 
   return (
