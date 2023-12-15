58233366c2253425fd9a2b396b6ca96ffd8e6775
강동욱 <ehddnr8563@gmail.com>
2023-12-16 06:18:01 +0900

    add: 언컨트롤, 컨트롤 컴포넌트

--- basic/src/deepDive/AppForm.jsx
+++ basic/src/deepDive/AppForm.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { useState } from 'react';
 
 export default function AppForm() {
-  const [form, setForm] = useState({ name: '', email: '' });
+  const [form, setForm] = useState({ name: '', email: '' }); // 연관된 데이터는 객체로 묶어서 상태 관리
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // 리프레쉬x
+    e.preventDefault();
     console.log(form);
   };
-
+  /**
+   * 리액트의 철학
+   *
+   * 모든 UI의 업데이트는 상태의 변경으로 부터 발생해야 한다.
+   * form에 있는 input 데이터는 사용자가 바로 수정하고 눈으로 바로 확인 가능하기 때문에 언 컨트롤 컴포넌트라고 한다.
+   * => 리액트에서 추구하는 원칙과 어긋난다. 리액트는 항상 UI의 업데이트는 리액트의 상태로 부터 발생되어야 한다.
+   * => 그래서 form을 사용할때는 useState를 이용해서 value는 그 상태의 값을 출력하고 onChange가 발생할 때 마다 그 상태를 업데이트 해줘야된다.
+   */
   const handleChange = (e) => {
     // console.log('name:', e.target.name);
     // console.log('value:', e.target.value);
@@ -37,10 +44,3 @@ export default function AppForm() {
     </form>
   );
 }
-
-// 리액트의 철학
-// 모든 UI의 업데이트는 상태의 변경으로 부터 발생해야 한다.
-
-// form에 있는 input 데이터는 사용자가 바로 수정하고 눈으로 바로 확인 가능하기 때문에 언 컨트롤 컴포넌트라고 한다.
-// => 리액트에서 추구하는 원칙과 어긋난다. 리액트는 항상 UI의 업데이트는 리액트의 상태로 부터 발생되어야 한다.
-// => 그래서 form을 사용할때는 useState를 이용해서 value는 그 상태의 값을 출력하고 onChange가 발생할 때 마다 그 상태를 업데이트 해줘야된다.
