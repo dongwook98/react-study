addc27069a865529b802c633265ec47ee3e7ee83
강동욱 <ehddnr8563@gmail.com>
2023-12-16 23:26:47 +0900

    add: tailwindCss

--- css/src/App.js
+++ css/src/App.js
@@ -4,6 +4,12 @@ import Button2 from './components/Button2';
 import StyledComponent from './StyledComponent';
 import TailwindComponent from './TailwindComponent';
 
+/**
+ * - 클래스명의 중복을 걱정하지 않고 스타일링 가능한 POST CSS
+ * - css파일을 만들지않고도 js파일안에서 우리만의 스타일링이 가능한 라이브러리 Styled-Components
+ * - JSX에서 클래스에 바로 스타일링이 가능한 TailwindCSS
+ */
+
 function App() {
   return (
     <>
--- css/src/TailwindComponent.jsx
+++ css/src/TailwindComponent.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 export default function TailwindComponent() {
   return (
     <div>
+      {/* command + i 하면 자동완성 */}
       <h1 className='text-8xl'>안녕!</h1>
-      <button className='bg-blue-500 rounded-xl px-2'>Button</button>
+      <button className='bg-blue-500 rounded-xl px-4'>Button</button>
     </div>
   );
 }
