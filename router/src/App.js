ff32a5e11ff4dc3000475df701d15c052e90b0c9
강동욱 <ehddnr8563@gmail.com>
2023-12-18 22:49:42 +0900

    add: 주석

--- router/src/App.js
+++ router/src/App.js
@@ -4,13 +4,14 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Root from './pages/Root';
 import Videos from './pages/Videos';
-import VideoDetail from './pages/VideoDatil';
+import VideoDetail from './pages/VideoDetail';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <NotFound />,
+    // children에 아울렛 등록
     children: [
       { index: true, element: <Home /> },
       { path: '/videos', element: <Videos /> },
@@ -20,7 +21,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router}>App</RouterProvider>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- router/src/pages/VideoDetail.jsx
+++ router/src/pages/VideoDetail.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-export default function VideoDatil() {
-  const { videoId } = useParams(); // 구조분해
+export default function VideoDetail() {
+  const { videoId } = useParams();
   console.log(videoId);
   return <div>VideoDetail {videoId}</div>;
 }
