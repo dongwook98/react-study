0db2471d8b4e321a4e60f7d3d2d118dd682a5ccf
강동욱 <ehddnr8563@gmail.com>
2023-12-19 00:07:07 +0900

    add: 리액트 쿼리 키 설정, stale 설정,

--- query/src/components/Products.jsx
+++ query/src/components/Products.jsx
@@ -8,10 +8,18 @@ export default function Products() {
     isLoading,
     error,
     data: products,
-  } = useQuery(['products'], async () => {
-    console.log('fetching....!');
-    return fetch(`data/products.json`).then((res) => res.json());
-  });
+  } = useQuery(
+    ['products', checked],
+    async () => {
+      console.log('fetching....!', checked);
+      return fetch(`data/${checked ? 'sale_' : ''}products.json`).then((res) =>
+        res.json()
+      );
+    },
+    {
+      staleTime: 1000 * 60 * 5,
+    }
+  );
   // const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
 
